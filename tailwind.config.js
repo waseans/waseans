c7c3b0ff5031b095545ed42b3da66cc757f0beb4
waseans <115150143+waseans@users.noreply.github.com>
2025-04-22 23:34:11 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+    darkMode: 'class',
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
+
+  
